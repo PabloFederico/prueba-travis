461937d9b34102ffbfc38ba959d88575ecd59ea6
PabloFederico <pablo.gabriel.federico@gmail.com>
2016-04-17 10:52:29 -0300

    cambiando2

--- src/main/java/ar/fiuba/tdd/tp/Main.java
+++ src/main/java/ar/fiuba/tdd/tp/Main.java
@@ -2,6 +2,6 @@ package ar.fiuba.tdd.tp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Cambiando texto nada mas");
+        System.out.println("change text nada mas");
     }
 }
